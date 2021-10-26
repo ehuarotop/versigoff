aba635a4c2ba7bc836857d98c63ea07457ff869d
Felix Eduardo Huaroto Pachas <edu.huaroto@gmail.com>
2021-10-26 13:42:00 -0500

    hot fix 2

--- scripts/cross_validation_all_datasets.py
+++ scripts/cross_validation_all_datasets.py
@@ -13,77 +13,77 @@ def main(n_samples):
 		samples = "12_samples"
 
 	print("CEDAR_BIASED")
-	os.system("""time python train_model.py --pairs_file pairs_files/{}/CEDAR_BIASED_pairs.txt \
+	os.system("""time python ../train_model.py --pairs_file pairs_files/{}/CEDAR_BIASED_pairs.txt \
 		--features_file CEDAR_BIASED_features.pk --dataset CEDAR --logfile CEDAR_BIASED.log\
 		--base_datasets_dir ../../master-thesis/datasets/CEDAR/""".format(samples))
 
 	print("BENGALI_BIASED")
-	os.system("""time python train_model.py --pairs_file pairs_files/{}/BENGALI_BIASED_pairs.txt \
+	os.system("""time python ../train_model.py --pairs_file pairs_files/{}/BENGALI_BIASED_pairs.txt \
 		--features_file BENGALI_BIASED_features.pk --dataset Bengali --logfile BENGALI_BIASED.log\
 		--base_datasets_dir ../../master-thesis/datasets/BHSig260/Bengali/""".format(samples))
 
 	print("HINDI_BIASED")
-	os.system("""time python train_model.py --pairs_file pairs_files/{}/HINDI_BIASED_pairs.txt \
+	os.system("""time python ../train_model.py --pairs_file pairs_files/{}/HINDI_BIASED_pairs.txt \
 		--features_file HINDI_BIASED_features.pk --dataset Hindi --logfile HINDI_BIASED.log\
 		--base_datasets_dir ../../master-thesis/datasets/BHSig260/Hindi/""".format(samples))
 
 	print("CEDAR_UNBIASED")
-	os.system("""time python train_model.py --pairs_file pairs_files/{}/CEDAR_UNBIASED_pairs.txt \
+	os.system("""time python ../train_model.py --pairs_file pairs_files/{}/CEDAR_UNBIASED_pairs.txt \
 		--features_file CEDAR_UNBIASED_features.pk --dataset CEDAR --logfile CEDAR_UNBIASED.log\
 		--base_datasets_dir ../../master-thesis/datasets/CEDAR/""".format(samples))
 
 	print("BENGALI_UNBIASED")
-	os.system("""time python train_model.py --pairs_file pairs_files/{}/BENGALI_UNBIASED_pairs.txt \
+	os.system("""time python ../train_model.py --pairs_file pairs_files/{}/BENGALI_UNBIASED_pairs.txt \
 		--features_file BENGALI_UNBIASED_features.pk --dataset Bengali --logfile BENGALI_UNBIASED.log\
 		--base_datasets_dir ../../master-thesis/datasets/BHSig260/Bengali_trimmed/""".format(samples))
 
 	print("HINDI_UNBIASED")
-	os.system("""time python train_model.py --pairs_file pairs_files/{}/HINDI_UNBIASED_pairs.txt \
+	os.system("""time python ../train_model.py --pairs_file pairs_files/{}/HINDI_UNBIASED_pairs.txt \
 		--features_file HINDI_UNBIASED_features.pk --dataset Hindi --logfile HINDI_UNBIASED.log\
 		--base_datasets_dir ../../master-thesis/datasets/BHSig260/Hindi_trimmed/""".format(samples))
 
 	print("CEDAR_UNBIASED_ROTATIONS")
-	os.system("""time python train_model.py --pairs_file pairs_files/{}/CEDAR_UNBIASED_ROTATIONS_pairs.txt \
+	os.system("""time python ../train_model.py --pairs_file pairs_files/{}/CEDAR_UNBIASED_ROTATIONS_pairs.txt \
 		--features_file CEDAR_UNBIASED_ROTATIONS_features.pk --dataset CEDAR --logfile CEDAR_UNBIASED_ROTATIONS.log\
 		--base_datasets_dir ../../master-thesis/datasets/CEDAR/""".format(samples))
 
 	print("BENGALI_UNBIASED_ROTATIONS")
-	os.system("""time python train_model.py --pairs_file pairs_files/{}/BENGALI_UNBIASED_ROTATIONS_pairs.txt \
+	os.system("""time python ../train_model.py --pairs_file pairs_files/{}/BENGALI_UNBIASED_ROTATIONS_pairs.txt \
 		--features_file BENGALI_UNBIASED_ROTATIONS_features.pk --dataset Bengali --logfile BENGALI_UNBIASED_ROTATIONS.log\
 		--base_datasets_dir ../../master-thesis/datasets/BHSig260/Bengali_trimmed_rotations/""".format(samples))
 
 	print("HINDI_UNBIASED_ROTATIONS")
-	os.system("""time python train_model.py --pairs_file pairs_files/{}/HINDI_UNBIASED_ROTATIONS_pairs.txt \
+	os.system("""time python ../train_model.py --pairs_file pairs_files/{}/HINDI_UNBIASED_ROTATIONS_pairs.txt \
 		--features_file HINDI_UNBIASED_ROTATIONS_features.pk --dataset Hindi --logfile HINDI_UNBIASED_ROTATIONS.log\
 		--base_datasets_dir ../../master-thesis/datasets/BHSig260/Hindi_trimmed_rotations/""".format(samples))
 
 	print("CEDAR_UNBIASED_SCALES")
-	os.system("""time python train_model.py --pairs_file pairs_files/{}/CEDAR_UNBIASED_SCALES_pairs.txt \
+	os.system("""time python ../train_model.py --pairs_file pairs_files/{}/CEDAR_UNBIASED_SCALES_pairs.txt \
 		--features_file CEDAR_UNBIASED_SCALES_features.pk --dataset CEDAR --logfile CEDAR_UNBIASED_SCALES.log\
 		--base_datasets_dir ../../master-thesis/datasets/CEDAR/""".format(samples))
 
 	print("BENGALI_UNBIASED_SCALES")
-	os.system("""time python train_model.py --pairs_file pairs_files/{}/BENGALI_UNBIASED_SCALES_pairs.txt \
+	os.system("""time python ../train_model.py --pairs_file pairs_files/{}/BENGALI_UNBIASED_SCALES_pairs.txt \
 		--features_file BENGALI_UNBIASED_SCALES_features.pk --dataset Bengali --logfile BENGALI_UNBIASED_SCALES.log\
 		--base_datasets_dir ../../master-thesis/datasets/BHSig260/Bengali_trimmed_scales/""".format(samples))
 
 	print("HINDI_UNBIASED_SCALES")
-	os.system("""time python train_model.py --pairs_file pairs_files/{}/HINDI_UNBIASED_SCALES_pairs.txt \
+	os.system("""time python ../train_model.py --pairs_file pairs_files/{}/HINDI_UNBIASED_SCALES_pairs.txt \
 		--features_file HINDI_UNBIASED_SCALES_features.pk --dataset Hindi --logfile HINDI_UNBIASED_SCALES.log\
 		--base_datasets_dir ../../master-thesis/datasets/BHSig260/Hindi_trimmed_scales/""".format(samples))
 
 	print("CEDAR_UNBIASED_ROTATION_SCALES")
-	os.system("""time python train_model.py --pairs_file pairs_files/{}/CEDAR_UNBIASED_ROTATION_SCALES_pairs.txt \
+	os.system("""time python ../train_model.py --pairs_file pairs_files/{}/CEDAR_UNBIASED_ROTATION_SCALES_pairs.txt \
 		--features_file CEDAR_UNBIASED_ROTATION_SCALES_features.pk --dataset CEDAR --logfile CEDAR_UNBIASED_ROTATION_SCALES.log\
 		--base_datasets_dir ../../master-thesis/datasets/CEDAR/""".format(samples))
 
 	print("BENGALI_UNBIASED_ROTATION_SCALES")
-	os.system("""time python train_model.py --pairs_file pairs_files/{}/BENGALI_UNBIASED_ROTATION_SCALES_pairs.txt \
+	os.system("""time python ../train_model.py --pairs_file pairs_files/{}/BENGALI_UNBIASED_ROTATION_SCALES_pairs.txt \
 		--features_file BENGALI_UNBIASED_ROTATION_SCALES_features.pk --dataset Bengali --logfile BENGALI_UNBIASED_ROTATION_SCALES.log\
 		--base_datasets_dir ../../master-thesis/datasets/BHSig260/Bengali_trimmed_rotation_scales/""".format(samples))
 
 	print("HINDI_UNBIASED_ROTATION_SCALES")
-	os.system("""time python train_model.py --pairs_file pairs_files/{}/HINDI_UNBIASED_ROTATION_SCALES_pairs.txt \
+	os.system("""time python ../train_model.py --pairs_file pairs_files/{}/HINDI_UNBIASED_ROTATION_SCALES_pairs.txt \
 		--features_file HINDI_UNBIASED_ROTATION_SCALES_features.pk --dataset Hindi --logfile HINDI_UNBIASED_ROTATION_SCALES.log\
 		--base_datasets_dir ../../master-thesis/datasets/BHSig260/Hindi_trimmed_rotation_scales/""".format(samples))
 
