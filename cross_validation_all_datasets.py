36475f6161c02becc5085183662b89084928e78e
Felix Eduardo Huaroto Pachas <edu.huaroto@gmail.com>
2021-11-03 00:48:01 -0500

    adding cross_val flag to cross validation script

--- cross_validation_all_datasets.py
+++ cross_validation_all_datasets.py
@@ -13,77 +13,77 @@ def main(n_samples):
 	print("CEDAR_BIASED")
 	os.system("""time python train_model.py --pairs_file pairs_files/{}/CEDAR_BIASED_pairs.txt \
 		--features_file CEDAR_BIASED_features.pk --dataset CEDAR --logfile CEDAR_BIASED.log\
-		--base_datasets_dir ../master-thesis/datasets/CEDAR/""".format(samples))
+		--base_datasets_dir ../master-thesis/datasets/CEDAR/ --cross_val""".format(samples))
 
 	print("BENGALI_BIASED")
 	os.system("""time python train_model.py --pairs_file pairs_files/{}/BENGALI_BIASED_pairs.txt \
 		--features_file BENGALI_BIASED_features.pk --dataset Bengali --logfile BENGALI_BIASED.log\
-		--base_datasets_dir ../master-thesis/datasets/BHSig260/Bengali/""".format(samples))
+		--base_datasets_dir ../master-thesis/datasets/BHSig260/Bengali/ --cross_val""".format(samples))
 
 	print("HINDI_BIASED")
 	os.system("""time python train_model.py --pairs_file pairs_files/{}/HINDI_BIASED_pairs.txt \
 		--features_file HINDI_BIASED_features.pk --dataset Hindi --logfile HINDI_BIASED.log\
-		--base_datasets_dir ../master-thesis/datasets/BHSig260/Hindi/""".format(samples))
+		--base_datasets_dir ../master-thesis/datasets/BHSig260/Hindi/ --cross_val""".format(samples))
 
 	print("CEDAR_UNBIASED")
 	os.system("""time python train_model.py --pairs_file pairs_files/{}/CEDAR_UNBIASED_pairs.txt \
 		--features_file CEDAR_UNBIASED_features.pk --dataset CEDAR --logfile CEDAR_UNBIASED.log\
-		--base_datasets_dir ../master-thesis/datasets/CEDAR/""".format(samples))
+		--base_datasets_dir ../master-thesis/datasets/CEDAR/ --cross_val""".format(samples))
 
 	print("BENGALI_UNBIASED")
 	os.system("""time python train_model.py --pairs_file pairs_files/{}/BENGALI_UNBIASED_pairs.txt \
 		--features_file BENGALI_UNBIASED_features.pk --dataset Bengali --logfile BENGALI_UNBIASED.log\
-		--base_datasets_dir ../master-thesis/datasets/BHSig260/Bengali_trimmed/""".format(samples))
+		--base_datasets_dir ../master-thesis/datasets/BHSig260/Bengali_trimmed/ --cross_val""".format(samples))
 
 	print("HINDI_UNBIASED")
 	os.system("""time python train_model.py --pairs_file pairs_files/{}/HINDI_UNBIASED_pairs.txt \
 		--features_file HINDI_UNBIASED_features.pk --dataset Hindi --logfile HINDI_UNBIASED.log\
-		--base_datasets_dir ../master-thesis/datasets/BHSig260/Hindi_trimmed/""".format(samples))
+		--base_datasets_dir ../master-thesis/datasets/BHSig260/Hindi_trimmed/ --cross_val""".format(samples))
 
 	print("CEDAR_UNBIASED_ROTATIONS")
 	os.system("""time python train_model.py --pairs_file pairs_files/{}/CEDAR_UNBIASED_ROTATIONS_pairs.txt \
 		--features_file CEDAR_UNBIASED_ROTATIONS_features.pk --dataset CEDAR --logfile CEDAR_UNBIASED_ROTATIONS.log\
-		--base_datasets_dir ../master-thesis/datasets/CEDAR/""".format(samples))
+		--base_datasets_dir ../master-thesis/datasets/CEDAR/ --cross_val""".format(samples))
 
 	print("BENGALI_UNBIASED_ROTATIONS")
 	os.system("""time python train_model.py --pairs_file pairs_files/{}/BENGALI_UNBIASED_ROTATIONS_pairs.txt \
 		--features_file BENGALI_UNBIASED_ROTATIONS_features.pk --dataset Bengali --logfile BENGALI_UNBIASED_ROTATIONS.log\
-		--base_datasets_dir ../master-thesis/datasets/BHSig260/Bengali_trimmed_rotations/""".format(samples))
+		--base_datasets_dir ../master-thesis/datasets/BHSig260/Bengali_trimmed_rotations/ --cross_val""".format(samples))
 
 	print("HINDI_UNBIASED_ROTATIONS")
 	os.system("""time python train_model.py --pairs_file pairs_files/{}/HINDI_UNBIASED_ROTATIONS_pairs.txt \
 		--features_file HINDI_UNBIASED_ROTATIONS_features.pk --dataset Hindi --logfile HINDI_UNBIASED_ROTATIONS.log\
-		--base_datasets_dir ../master-thesis/datasets/BHSig260/Hindi_trimmed_rotations/""".format(samples))
+		--base_datasets_dir ../master-thesis/datasets/BHSig260/Hindi_trimmed_rotations/ --cross_val""".format(samples))
 
 	print("CEDAR_UNBIASED_SCALES")
 	os.system("""time python train_model.py --pairs_file pairs_files/{}/CEDAR_UNBIASED_SCALES_pairs.txt \
 		--features_file CEDAR_UNBIASED_SCALES_features.pk --dataset CEDAR --logfile CEDAR_UNBIASED_SCALES.log\
-		--base_datasets_dir ../master-thesis/datasets/CEDAR/""".format(samples))
+		--base_datasets_dir ../master-thesis/datasets/CEDAR/ --cross_val""".format(samples))
 
 	print("BENGALI_UNBIASED_SCALES")
 	os.system("""time python train_model.py --pairs_file pairs_files/{}/BENGALI_UNBIASED_SCALES_pairs.txt \
 		--features_file BENGALI_UNBIASED_SCALES_features.pk --dataset Bengali --logfile BENGALI_UNBIASED_SCALES.log\
-		--base_datasets_dir ../master-thesis/datasets/BHSig260/Bengali_trimmed_scales/""".format(samples))
+		--base_datasets_dir ../master-thesis/datasets/BHSig260/Bengali_trimmed_scales/ --cross_val""".format(samples))
 
 	print("HINDI_UNBIASED_SCALES")
 	os.system("""time python train_model.py --pairs_file pairs_files/{}/HINDI_UNBIASED_SCALES_pairs.txt \
 		--features_file HINDI_UNBIASED_SCALES_features.pk --dataset Hindi --logfile HINDI_UNBIASED_SCALES.log\
-		--base_datasets_dir ../master-thesis/datasets/BHSig260/Hindi_trimmed_scales/""".format(samples))
+		--base_datasets_dir ../master-thesis/datasets/BHSig260/Hindi_trimmed_scales/ --cross_val""".format(samples))
 
 	print("CEDAR_UNBIASED_ROTATION_SCALES")
 	os.system("""time python train_model.py --pairs_file pairs_files/{}/CEDAR_UNBIASED_ROTATION_SCALES_pairs.txt \
 		--features_file CEDAR_UNBIASED_ROTATION_SCALES_features.pk --dataset CEDAR --logfile CEDAR_UNBIASED_ROTATION_SCALES.log\
-		--base_datasets_dir ../master-thesis/datasets/CEDAR/""".format(samples))
+		--base_datasets_dir ../master-thesis/datasets/CEDAR/ --cross_val""".format(samples))
 
 	print("BENGALI_UNBIASED_ROTATION_SCALES")
 	os.system("""time python train_model.py --pairs_file pairs_files/{}/BENGALI_UNBIASED_ROTATION_SCALES_pairs.txt \
 		--features_file BENGALI_UNBIASED_ROTATION_SCALES_features.pk --dataset Bengali --logfile BENGALI_UNBIASED_ROTATION_SCALES.log\
-		--base_datasets_dir ../master-thesis/datasets/BHSig260/Bengali_trimmed_rotation_scales/""".format(samples))
+		--base_datasets_dir ../master-thesis/datasets/BHSig260/Bengali_trimmed_rotation_scales/ --cross_val""".format(samples))
 
 	print("HINDI_UNBIASED_ROTATION_SCALES")
 	os.system("""time python train_model.py --pairs_file pairs_files/{}/HINDI_UNBIASED_ROTATION_SCALES_pairs.txt \
 		--features_file HINDI_UNBIASED_ROTATION_SCALES_features.pk --dataset Hindi --logfile HINDI_UNBIASED_ROTATION_SCALES.log\
-		--base_datasets_dir ../master-thesis/datasets/BHSig260/Hindi_trimmed_rotation_scales/""".format(samples))
+		--base_datasets_dir ../master-thesis/datasets/BHSig260/Hindi_trimmed_rotation_scales/ --cross_val""".format(samples))
 
 if __name__ == "__main__":
 	main()
--- cross_validation_all_datasets_random_forgeries.py
+++ cross_validation_all_datasets_random_forgeries.py
@@ -6,77 +6,77 @@ def main():
 	print("CEDAR_BIASED")
 	os.system("""time python train_model.py --pairs_file pairs_files/all_samples_random_forgeries/CEDAR_BIASED_pairs.txt \
 		--features_file CEDAR_BIASED_features.pk --dataset CEDAR --logfile CEDAR_BIASED.log\
-		--base_datasets_dir ../master-thesis/datasets/CEDAR/""")
+		--base_datasets_dir ../master-thesis/datasets/CEDAR/ --cross_val""")
 
 	print("BENGALI_BIASED")
 	os.system("""time python train_model.py --pairs_file pairs_files/all_samples_random_forgeries/BENGALI_BIASED_pairs.txt \
 		--features_file BENGALI_BIASED_features.pk --dataset Bengali --logfile BENGALI_BIASED.log\
-		--base_datasets_dir ../master-thesis/datasets/BHSig260/Bengali/""")
+		--base_datasets_dir ../master-thesis/datasets/BHSig260/Bengali/ --cross_val""")
 
 	print("HINDI_BIASED")
 	os.system("""time python train_model.py --pairs_file pairs_files/all_samples_random_forgeries/HINDI_BIASED_pairs.txt \
 		--features_file HINDI_BIASED_features.pk --dataset Hindi --logfile HINDI_BIASED.log\
-		--base_datasets_dir ../master-thesis/datasets/BHSig260/Hindi/""")
+		--base_datasets_dir ../master-thesis/datasets/BHSig260/Hindi/ --cross_val""")
 
 	print("CEDAR_UNBIASED")
 	os.system("""time python train_model.py --pairs_file pairs_files/all_samples_random_forgeries/CEDAR_UNBIASED_pairs.txt \
 		--features_file CEDAR_UNBIASED_features.pk --dataset CEDAR --logfile CEDAR_UNBIASED.log\
-		--base_datasets_dir ../master-thesis/datasets/CEDAR/""")
+		--base_datasets_dir ../master-thesis/datasets/CEDAR/ --cross_val""")
 
 	print("BENGALI_UNBIASED")
 	os.system("""time python train_model.py --pairs_file pairs_files/all_samples_random_forgeries/BENGALI_UNBIASED_pairs.txt \
 		--features_file BENGALI_UNBIASED_features.pk --dataset Bengali --logfile BENGALI_UNBIASED.log\
-		--base_datasets_dir ../master-thesis/datasets/BHSig260/Bengali_trimmed/""")
+		--base_datasets_dir ../master-thesis/datasets/BHSig260/Bengali_trimmed/ --cross_val""")
 
 	print("HINDI_UNBIASED")
 	os.system("""time python train_model.py --pairs_file pairs_files/all_samples_random_forgeries/HINDI_UNBIASED_pairs.txt \
 		--features_file HINDI_UNBIASED_features.pk --dataset Hindi --logfile HINDI_UNBIASED.log\
-		--base_datasets_dir ../master-thesis/datasets/BHSig260/Hindi_trimmed/""")
+		--base_datasets_dir ../master-thesis/datasets/BHSig260/Hindi_trimmed/ --cross_val""")
 
 	print("CEDAR_UNBIASED_ROTATIONS")
 	os.system("""time python train_model.py --pairs_file pairs_files/all_samples_random_forgeries/CEDAR_UNBIASED_ROTATIONS_pairs.txt \
 		--features_file CEDAR_UNBIASED_ROTATIONS_features.pk --dataset CEDAR --logfile CEDAR_UNBIASED_ROTATIONS.log\
-		--base_datasets_dir ../master-thesis/datasets/CEDAR/""")
+		--base_datasets_dir ../master-thesis/datasets/CEDAR/ --cross_val""")
 
 	print("BENGALI_UNBIASED_ROTATIONS")
 	os.system("""time python train_model.py --pairs_file pairs_files/all_samples_random_forgeries/BENGALI_UNBIASED_ROTATIONS_pairs.txt \
 		--features_file BENGALI_UNBIASED_ROTATIONS_features.pk --dataset Bengali --logfile BENGALI_UNBIASED_ROTATIONS.log\
-		--base_datasets_dir ../master-thesis/datasets/BHSig260/Bengali_trimmed_rotations/""")
+		--base_datasets_dir ../master-thesis/datasets/BHSig260/Bengali_trimmed_rotations/ --cross_val""")
 
 	print("HINDI_UNBIASED_ROTATIONS")
 	os.system("""time python train_model.py --pairs_file pairs_files/all_samples_random_forgeries/HINDI_UNBIASED_ROTATIONS_pairs.txt \
 		--features_file HINDI_UNBIASED_ROTATIONS_features.pk --dataset Hindi --logfile HINDI_UNBIASED_ROTATIONS.log\
-		--base_datasets_dir ../master-thesis/datasets/BHSig260/Hindi_trimmed_rotations/""")
+		--base_datasets_dir ../master-thesis/datasets/BHSig260/Hindi_trimmed_rotations/ --cross_val""")
 
 	print("CEDAR_UNBIASED_SCALES")
 	os.system("""time python train_model.py --pairs_file pairs_files/all_samples_random_forgeries/CEDAR_UNBIASED_SCALES_pairs.txt \
 		--features_file CEDAR_UNBIASED_SCALES_features.pk --dataset CEDAR --logfile CEDAR_UNBIASED_SCALES.log\
-		--base_datasets_dir ../master-thesis/datasets/CEDAR/""")
+		--base_datasets_dir ../master-thesis/datasets/CEDAR/ --cross_val""")
 
 	print("BENGALI_UNBIASED_SCALES")
 	os.system("""time python train_model.py --pairs_file pairs_files/all_samples_random_forgeries/BENGALI_UNBIASED_SCALES_pairs.txt \
 		--features_file BENGALI_UNBIASED_SCALES_features.pk --dataset Bengali --logfile BENGALI_UNBIASED_SCALES.log\
-		--base_datasets_dir ../master-thesis/datasets/BHSig260/Bengali_trimmed_scales/""")
+		--base_datasets_dir ../master-thesis/datasets/BHSig260/Bengali_trimmed_scales/ --cross_val""")
 
 	print("HINDI_UNBIASED_SCALES")
 	os.system("""time python train_model.py --pairs_file pairs_files/all_samples_random_forgeries/HINDI_UNBIASED_SCALES_pairs.txt \
 		--features_file HINDI_UNBIASED_SCALES_features.pk --dataset Hindi --logfile HINDI_UNBIASED_SCALES.log\
-		--base_datasets_dir ../master-thesis/datasets/BHSig260/Hindi_trimmed_scales/""")
+		--base_datasets_dir ../master-thesis/datasets/BHSig260/Hindi_trimmed_scales/ --cross_val""")
 
 	print("CEDAR_UNBIASED_ROTATION_SCALES")
 	os.system("""time python train_model.py --pairs_file pairs_files/all_samples_random_forgeries/CEDAR_UNBIASED_ROTATION_SCALES_pairs.txt \
 		--features_file CEDAR_UNBIASED_ROTATION_SCALES_features.pk --dataset CEDAR --logfile CEDAR_UNBIASED_ROTATION_SCALES.log\
-		--base_datasets_dir ../master-thesis/datasets/CEDAR/""")
+		--base_datasets_dir ../master-thesis/datasets/CEDAR/ --cross_val""")
 
 	print("BENGALI_UNBIASED_ROTATION_SCALES")
 	os.system("""time python train_model.py --pairs_file pairs_files/all_samples_random_forgeries/BENGALI_UNBIASED_ROTATION_SCALES_pairs.txt \
 		--features_file BENGALI_UNBIASED_ROTATION_SCALES_features.pk --dataset Bengali --logfile BENGALI_UNBIASED_ROTATION_SCALES.log\
-		--base_datasets_dir ../master-thesis/datasets/BHSig260/Bengali_trimmed_rotation_scales/""")
+		--base_datasets_dir ../master-thesis/datasets/BHSig260/Bengali_trimmed_rotation_scales/ --cross_val""")
 
 	print("HINDI_UNBIASED_ROTATION_SCALES")
 	os.system("""time python train_model.py --pairs_file pairs_files/all_samples_random_forgeries/HINDI_UNBIASED_ROTATION_SCALES_pairs.txt \
 		--features_file HINDI_UNBIASED_ROTATION_SCALES_features.pk --dataset Hindi --logfile HINDI_UNBIASED_ROTATION_SCALES.log\
-		--base_datasets_dir ../master-thesis/datasets/BHSig260/Hindi_trimmed_rotation_scales/""")
+		--base_datasets_dir ../master-thesis/datasets/BHSig260/Hindi_trimmed_rotation_scales/ --cross_val""")
 
 if __name__ == "__main__":
 	main()
