c0450080ed64241860a8ef6b38fc474e81705767
Felix Eduardo Huaroto Pachas <edu.huaroto@gmail.com>
2021-12-12 05:18:54 -0500

    hot fix on pca clip

--- feature_generation.py
+++ feature_generation.py
@@ -323,6 +323,7 @@ def generate_features(df, imgs, features_file):
 	pca = PCA(n_components=128)
 	clip_features = np.array([x for x in df_clip["clip_features"].values])
 	clip_features = pca.fit_transform(clip_features)
+	clip_features = [x for in in clip_features]
 	df_clip = df_clip.assign(clip_features=clip_features)
 
 	#Generating final features
