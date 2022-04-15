14ab5e920fde4e93f7ac95ec72d7eb8e8cd3274e
Felix Eduardo Huaroto Pachas <edu.huaroto@gmail.com>
2022-04-15 12:53:16 -0500

    considering time tracking for PCA procedure

--- feature_generation.py
+++ feature_generation.py
@@ -325,11 +325,13 @@ def generate_features(df, imgs, features_file, pca=False):
 
 	#Applying PCA over clip_features
 	if pca:
+		start_pca = time.time()
 		pca = PCA(n_components=128)
 		clip_features = np.array([x for x in df_clip["clip_features"].values])
 		clip_features = pca.fit_transform(clip_features)
 		clip_features = [x for x in clip_features]
 		df_clip = df_clip.assign(clip_features=clip_features)
+		print("PCA procedure takes {:.2f} seconds".format(time.time() - start_pca))	
 
 	#Generating final features
 	print("Generating final features")
